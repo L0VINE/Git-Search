1f9d3e3b1e9a721c595f0cd8228793fcf6eb9047
Awadh-Awadh <awadh.awadhawadh.awadh@student.moringaschool.com>
2021-07-25 16:58:37 +0300

    add html for the hero section and styling

--- src/app/hero/hero.component.ts
+++ src/app/hero/hero.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./hero.component.css']
 })
 export class HeroComponent implements OnInit {
+  imagePath = "/assets/cubes.png"
 
   constructor() { }
 
