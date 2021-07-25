ed2e11f669c242259cf3e8194abe73d6650ff68d
Awadh-Awadh <awadh.awadhawadh.awadh@student.moringaschool.com>
2021-07-25 23:35:08 +0300

    add a get request to the rest api

--- src/app/repo.service.ts
+++ src/app/repo.service.ts
@@ -1,11 +1,19 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import{}
+import { IRepos } from './repos';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RepoService {
+  profileUrl = 'https://api.github.com/users/'
+  constructor(private http:HttpClient) { }
 
-  constructor() { }
+
+
+ public getRepos(userName:string):Observable<IRepos[]>{
+   return this.http.get<IRepos[]>(this.profileUrl+userName+'/repos')
+ }
+
 }
