5adee75e7e77ee8e59f8cdf064d3a5168907aef3
Awadh-Awadh <awadh.awadhawadh.awadh@student.moringaschool.com>
2021-07-25 23:25:39 +0300

    add repository interface

---
+++ src/app/repos.ts
@@ -0,0 +1,7 @@
+export interface IRepos{
+  name:string
+  description:string
+  html_url:string
+  created_at:Date
+  homepage:string
+}
--- src/app/repos/repos.component.ts
+++ src/app/repos/repos.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class ReposComponent implements OnInit {
 
   constructor() { }
-
+  repos:[] = []
   ngOnInit(): void {
   }
 
