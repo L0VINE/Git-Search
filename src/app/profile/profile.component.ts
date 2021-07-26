c0537e34ed783ee4304782f7253e2eabf791e643
Awadh-Awadh <awadh.awadhawadh.awadh@student.moringaschool.com>
2021-07-26 13:25:26 +0300

    replace subscribe with a promise

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -13,18 +13,38 @@ export class ProfileComponent implements OnInit {
   userName:string = ''
   profiles: any = [];
   repos: any = [];
+  errorMsg;
 
   constructor(private profileService:ProfilesService, private repoService:RepoService, private http:HttpClient) { }
 
   ngOnInit(): void {
-    this.profileService.getProfile(this.userName).subscribe(data=>{
-    this.profiles = data;
-     })
 }
-getUsers(userName){
-  this.profileService.getProfile(this.userName).subscribe(data=>{
-    this.profiles = data;
-     })
+// public getUsers(userName){
+//  let promise = new Promise(resolve,reject)=>{
+//    this.profileService.getProfile(this.userName).toPromise().then(response=>{
+//      this.profiles = response;
+//      resolve()
+//     },error=>{
+//       this.errorMsg = "An error was Encountered"
+//     }
+     
+//    })
+//    return promise
+//  }
+
+public getUsers(event) {
+   let promise = new Promise((resolve: any , reject) => {
+   this.profileService.getProfile (this.userName).toPromise().then(response => {
+     this.profiles = response;
+      resolve();
+    },
+    error => {
+      this.errorMsg = 'An error was encountered';
+      reject(error)
+    }
+  );
+  });
+  return promise;
 }
 getDetails(userName){
   this.repoService.getRepos(this.userName).subscribe(data=>{
